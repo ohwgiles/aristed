0fca5cdc25bf97fd491be14bd110ec4d05065000
Oliver Giles <ohw.giles@gmail.com>
2013-01-17 14:24:27 +0800

    use logging fifo

--- src/log.cpp
+++ src/log.cpp
@@ -1,26 +1,60 @@
 #include "log.hpp"
 #include <QString>
 #include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fstream>
+
+#define COLOUR_TEAL    "\033[0;36m"
+#define COLOUR_YELLOW  "\033[1;33m"
+#define COLOUR_GREEN   "\033[0;32m"
+#define COLOUR_RED     "\033[1;31m"
+#define COLOUR_WHITE   "\033[1;37m"
+#define COLOUR_NORMAL  "\033[m"
+
+#define LOGSTR(loglevel) ( \
+	loglevel==_Log::TRACE ? "TRACE" :\
+	loglevel==_Log::DEBUG ? "DEBUG" :\
+	loglevel==_Log::INFO  ? "INFO" :\
+	loglevel==_Log::ERROR ? "ERROR" : 0)
+
+#define COLOR(loglevel) ( \
+	loglevel==_Log::TRACE ? COLOUR_TEAL :\
+	loglevel==_Log::DEBUG ? COLOUR_YELLOW :\
+	loglevel==_Log::INFO  ? COLOUR_GREEN :\
+	loglevel==_Log::ERROR ? COLOUR_RED : 0)
 
 namespace {
 
-const char* LogLevelToString(_Log::LogLevel l) {
-	switch(l) {
-	case _Log::TRACE: return "TRACE";
-	case _Log::DEBUG: return "DEBUG";
-	case _Log::INFO:  return "INFO";
-	case _Log::ERROR: return "ERROR";
-	case _Log::FATAL: return "FATAL";
+class LogFifo {
+public:
+	static std::fstream* stream;
+	LogFifo() {
+		unlink("/tmp/aristed.log");
+		mkfifo("/tmp/aristed.log", 0600);
+		stream = new std::fstream("/tmp/aristed.log");
 	}
-	return 0;
-}
+	~LogFifo() {
+		delete stream;
+	}
+};
+
+std::fstream* LogFifo::stream;
+LogFifo __static_logfifo;
 
 }
 
 _Log::~_Log() {
-	std::cout << "[" << LogLevelToString(lvl) << "] " << fn << ":" << line << " " << str() << std::endl;
+#ifdef NDEBUG
+	// only called for errors > ERROR
+	std::cout << "Error: " << str() << std::endl;
+#else
+	(*LogFifo::stream) << "[" << COLOR(lvl) << LOGSTR(lvl) << COLOUR_NORMAL << "] " << fn << ":" << line << " " << str() << std::endl;
+#endif
 }
 
+// handle QStrings
 std::ostream& operator<<(std::ostream& os, const QString& qs) {
 	os << static_cast<const char*>(qs.toLocal8Bit().constData());
 	return os;
--- src/log.hpp
+++ src/log.hpp
@@ -25,9 +25,17 @@ std::ostream& operator<<(std::ostream& os, const QString& qs);
 
 #define  ae_fatal(msg) { _Log(_Log::ERROR, __PRETTY_FUNCTION__, __LINE__).stream() << msg; exit(1); }
 #define  ae_error(msg) _Log(_Log::ERROR, __PRETTY_FUNCTION__, __LINE__).stream() << msg
+
+#ifdef NDEBUG
+#define   ae_info(msg)
+#define  ae_debug(msg)
+#define  ae_trace(msg)
+#define ae_assert(cond)
+#else
 #define   ae_info(msg) _Log(_Log::INFO , __PRETTY_FUNCTION__, __LINE__).stream() << msg
 #define  ae_debug(msg) _Log(_Log::DEBUG, __PRETTY_FUNCTION__, __LINE__).stream() << msg
 #define  ae_trace(msg) _Log(_Log::TRACE, __PRETTY_FUNCTION__, __LINE__).stream() << msg
 #define ae_assert(cond) if(!(cond)) { ae_fatal("Assertion " # cond " failed "); }
+#endif
 
 #endif // Log_HPP
